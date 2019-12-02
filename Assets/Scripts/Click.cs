b0bc7031eab47f2c30e22ff22bad8b1763216858
Pilou <pierre-louis.franques@epitech.eu>
2019-12-02 21:58:24 +0100

    V1 map + V1 mov cam + archi

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    private bool _isClicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        _isClicked = true;
+        Debug.Log(this.name);
+    }
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public float panSpeed = 100f;
+    public float scrollSpeed = 10f;
+
+    private float panBorder = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("z") || Input.mousePosition.y >= Screen.height - panBorder)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorder)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorder)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("q") || Input.mousePosition.x <= panBorder)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100 * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -330, 330);
+        pos.y = Mathf.Clamp(pos.y, 30, 500);
+        pos.z = Mathf.Clamp(pos.z, -560, 200);
+
+
+        transform.position = pos;
+    }
+}
