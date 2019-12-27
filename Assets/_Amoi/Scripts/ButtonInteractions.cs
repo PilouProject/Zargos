e8da227b3ff84c290aaf5f24e6787746b3a39a21
Pilou <pierre-louis.franques@epitech.eu>
2019-12-27 22:08:40 +0100

    Border game + Asset icon/cursor + start all menu

---
+++ Assets/_Amoi/Scripts/ButtonInteractions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteractions : MonoBehaviour
+{
+
+    private GameObject _optionMenu;
+    private GameObject _rulesMenu;
+    private GameObject _principalMenu;
+    private GameObject _startMenu;
+    private GameObject _pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _optionMenu = GameObject.Find("OptionMenu");
+        _rulesMenu = GameObject.Find("RulesMenu");
+        _principalMenu = GameObject.Find("PrincipalMenu");
+        _startMenu = GameObject.Find("StartMenu");
+        _pauseMenu = GameObject.Find("PauseMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnQuitButton()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void OnOptionButton()
+    {
+        _optionMenu.SetActive(true);
+    }
+
+    public void OnRulesButton()
+    {
+        _rulesMenu.SetActive(true);
+    }
+}
---
+++ Assets/_Amoi/Scripts/MoveCamera.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float rotateSpeed = 2f;
+    public float panSpeed = 100f;
+    public float scrollSpeed = 10f;
+
+    private float _panBorder = 20f;
+    private Vector3 _pos;
+    private Vector3 _posBonderies;
+    private float _mouseX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _posBonderies = transform.position;
+        _pos = Vector3.zero;
+
+        if (Input.GetKey("z") || Input.mousePosition.y >= Screen.height - _panBorder)
+        {
+            _pos += Vector3.forward * panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= _panBorder)
+        {
+            _pos -= Vector3.forward * panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - _panBorder)
+        {
+            _pos += Vector3.right * panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("q") || Input.mousePosition.x <= _panBorder)
+        {
+            _pos -= Vector3.right * panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            if (Input.mousePosition.x != _mouseX)
+            {
+                var cameraRotationY = (Input.mousePosition.x - _mouseX) * rotateSpeed * Time.deltaTime;
+                transform.Rotate(0, cameraRotationY, 0, Space.World);
+            }
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        _pos.y -= scroll * scrollSpeed * 100 * Time.deltaTime;
+
+        _posBonderies.x = Mathf.Clamp(_posBonderies.x, -700, 700);
+        _posBonderies.y = Mathf.Clamp(_posBonderies.y, 30, 500);
+        _posBonderies.z = Mathf.Clamp(_posBonderies.z, -700, 700);
+
+
+        transform.position = _posBonderies;
+        transform.Translate(_pos, Space.Self);
+
+        //transform.localPosition = _pos;
+        _mouseX = Input.mousePosition.x;
+    }
+}
