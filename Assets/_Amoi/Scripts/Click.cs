ee61b40d1a546291420ef70a3e1dad124336ff17
Pilou <pierre-louis.franques@epitech.eu>
2020-02-15 22:41:02 +0100

    Highlight not finish

--- Assets/_Amoi/Scripts/Click.cs
+++ Assets/_Amoi/Scripts/Click.cs
@@ -8,17 +8,43 @@ public class Click : MonoBehaviour
 
     private Transform _spawnPoint;
 
+    private GameObject _ownGroundHighlight;
+    private Renderer _rendererHighlight;
+    private Color _colorStart;
+    private Color _colorEnd;
+    private float t;
+
+
     // Start is called before the first frame update
     void Start()
     {
         _spawnPoint = transform.GetChild(0);
+
+        //_ownGroundHighlight = transform.GetChild(1).gameObject;
+        _ownGroundHighlight = GameObject.Find("Ground");
+        _rendererHighlight = _ownGroundHighlight.GetComponent<Renderer>();
+        //ColorUtility.TryParseHtmlString("#55FF70", out _colorStart);
+        //ColorUtility.TryParseHtmlString("#30A242", out _colorEnd);
+        //_colorStart = new Color(48, 161, 65);
+        //_colorEnd = new Color(85, 255, 112);
+        _colorStart = new Color(0, 0, 0);
+        _colorEnd = new Color(255, 255, 255);
+
         //Physics.gravity = new Vector3(0, -9.81f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Color.Lerp(_colorStart, _colorEnd, t));
+        //_rendererHighlight.material.SetColor("_LineColor", Color.Lerp(_colorStart, _colorEnd, Mathf.PingPong(Time.time, 1)));
+        _rendererHighlight.material.color = Color.Lerp(_colorStart, _colorEnd, t);
+        if (t < 1)
+        {
+            t += Time.deltaTime / 5;
+        }
+        else
+            t = 0;
     }
 
     private void OnMouseOver()
